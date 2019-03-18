c405abe47de205786ade597ed869ba7990f256dc
Yobi Livingstone <39736534+Dupeshway@users.noreply.github.com>
2019-03-18 16:39:12 +0000

    Add files via upload

---
+++ open_file.py
@@ -0,0 +1,10 @@
+# opening and reading the text file as strings
+
+def open_file(file):
+    '''reads in the file and prepares it for formatting
+    input= text file
+    output= reads the file and stores as strings ready to format
+    '''
+
+    with open("dummyfile.txt", "r") as f:
+        file= f.read().splitlines()
