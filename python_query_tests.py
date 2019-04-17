e3cfe0ea4df50093bdb76b90a69050860c84cf7f
mmarto27 <48446427+mmarto27@users.noreply.github.com>
2019-04-17 16:57:49 +0100

    Update python_query_tests.py

--- python_query_tests.py
+++ python_query_tests.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
+
+#basic python wrapper for SQL queries
+'''
+Program:
+File:
+Version:
+Date:
+Function: to wrap SQL queries in python language
+Author: Yobi Livingstone
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description:
+--------------------------------------------------------------------------
+Usage:
+--------------------------------------------------------------------------
+Revision History:
+--------------------------------------------------------------------------
+Import libraries
+'''
 import pymysql.cursors
 
 # Set parameters
@@ -8,16 +27,18 @@ dbuser   = "ly001"   # Ask a demonstrator
 dbpass   = "7#b8tpkum"   # Ask a demonstrator
 port     = 3306
 
-# Create SQL statement to find number of human protein entries
-sql      = "select *, from GENE"
+
+# Create search function
+def search_function(output_type, input_type, input_value):
+  sql = "select" + output_type + "where" + input_type + " == " + input_value   
 
 # Connect to the database
 db = pymysql.connect(host=dbhost, port=port, user=dbuser, passwd=dbpass, db=dbname)
 
 # Create a cursor and execute the SQL on it
 cursor = db.cursor()
-nrows  = cursor.execute(sql)
+data  = cursor.execute(sql)
 
 # We know there is only one row returned to fetch it and print the data
-data   = cursor.fetchone()
-print ("There were ", data[0])
+output_value   = cursor.fetchone()
+print (data[0])
