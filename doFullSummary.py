0e7b7c4aa82c9516a92f43de65b634e86c42d412
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-04-21 19:18:39 -0400

    Update doFullSummary.py

--- doFullSummary.py
+++ doFullSummary.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 #from database_layer import db_summary
+"""
+Program:Database Summary
+File:
+Version:
+Date:
+Function: 
+Author: Margherita Martorana, Documentation(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description:
+--------------------------------------------------------------------------
+Usage: 
+--------------------------------------------------------------------------
+Import libraries: 
+"""
 
 def doFullSummary():
+     """" Fuction for Full summary of Database   """
      full_summary= db_summary()
