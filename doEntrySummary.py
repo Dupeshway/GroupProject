0e8cd043eb2800c6206ab2ccbec757f4ead27ce5
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-04-21 21:31:13 -0400

    Update doEntrySummary.py

--- doEntrySummary.py
+++ doEntrySummary.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 #from database_layer import db_query
+"""
+Program:Import query from the database layer
+File:
+Version:
+Date:
+Author: Margherita Martorana, Documentation(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description:
+--------------------------------------------------------------------------
+Usage: Import from the database layer
+--------------------------------------------------------------------------
+Import libraries: configFile
 import configFile
+"""
 
 def doEntrySummary(input_type, input_value):
+     """ Input values and type if in infoList reutuen listed in database 
+               input_type= 
+               input_value= """
      if input_type in infoList:
           return db_query('(*)', input_type, input_value)
      else:
