8d61a94c0a981d2bbd5cab2ccc16ed6f91e55fce
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-04-26 14:28:08 -0400

    Update doCodingRegionsAndDNA.py

--- doCodingRegionsAndDNA.py
+++ doCodingRegionsAndDNA.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python3
 #from database_layer import db_query
+
+"""
+Program:Coding Regions And DNA
+File:
+Version:
+Date:
+Author: Margherita Martorana, Documentation(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description:
+--------------------------------------------------------------------------
+Usage: Import from the database layer
+--------------------------------------------------------------------------
+Import libraries: configFile
+import configFile
+"""
+
 import configFile
 
 def doCodingRegionsAndDNA(input_type, input_value):
+     """ Function for DNA coding Resgion and DNA
+          input_type=
+          input_value=
+          returnsequenceDNA if in list
+          
+          """
      if input_type in infoList:
           sequenceDNA = db_query(dbArg1, input_type, input_value)
 
