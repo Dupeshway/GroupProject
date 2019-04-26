37e261ae332291ef17b48b443c6b8460f3a9a804
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-04-26 14:34:23 -0400

    Update doEntrySummary.py

--- doEntrySummary.py
+++ doEntrySummary.py
@@ -21,6 +21,10 @@ import configFile
 def doEntrySummary(input_type, input_value):
           """  Function  for Entry Summary
                input_type= 
+                    infoType1 = 'gene_id'
+                    infoType2 = 'acc_number'
+                    infoType3 = 'product_name'
+                    infoType4 = 'chr_location'
                input_value= 
                in info list returns query"""
      if input_type in infoList:
