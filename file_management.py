d860e60e69aea79b02d47aa5a73dfd0fbabf37f9
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-04 13:51:41 +0100

    Update file_management.py

--- file_management.py
+++ file_management.py
@@ -1,8 +1,27 @@
+!/usr/bin/env python3
+
+"""
+Program:File Management 
+File:file_management.py
+Version:1.0
+Date:May-4-2019
+Author: Yobi Livingstone, Documentation headers(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description: 
+--------------------------------------------------------------------------
+Usage: 
+--------------------------------------------------------------------------
+Import libraries:
+"""
+
+""" File Management Module"""
 
 class file_management:
 
     from config import config as cg
 
+    """Parsing"""
     def open_file(r_file):
         '''Input: text file of chromosome
         Output: string of each line from input file returned for parsing
@@ -12,7 +31,7 @@ class file_management:
             raw_data = f.read().splitlines()
         return raw_data
 
-
+    """ Empty File"""
     def wipe_file(w_file):
         '''Wipe a file
         input:w_file to clear
@@ -22,7 +41,7 @@ class file_management:
         f = open(w_file, 'a+')
         f.truncate(0) 
 
-
+        """Write file parameter"""
     def write_file(datafile, w_file):
         '''Writes to the file parameter
         input 1: opens read file, defined in config, MUST BE SINGLE STRING
@@ -32,6 +51,8 @@ class file_management:
         f = open(w_file,'a+')
         f.write(datafile)
         print('Data written to:', w_file)
+
+        """ Write List to File Parameter"""
 
     def write_list(datafile, w_file):
         '''Writes to the file parameter
