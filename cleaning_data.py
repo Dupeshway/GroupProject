f83929c4099e29e1c9df49538ea8c9dd83daa01f
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-04 13:14:23 +0100

    Update cleaning_data.py

--- cleaning_data.py
+++ cleaning_data.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 #Yobi Livingstone
+
+"""
+Program:Cleaning Data
+File:cleaning_data.py
+Version:1.0
+Date:May-4-2019
+Author: Yobi Livingstone, Documentation headers(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description: Cleaning Raw data to make data easier to handle
+--------------------------------------------------------------------------
+Usage: 
+--------------------------------------------------------------------------
+Import libraries: re
+"""
+
 import re
+
+""" Cleaning Module """"
 
 class clean_data:
     '''Every function is intended to join, remove, replace iterables (strings, lists etc)
     to make the data easier to handle after capturing/parsing'''
 
-
+""" Joining Raw Data"""
+    
     def join_strings(raw_data):
         '''Join strings
         input: raw_data be multiple strings or lists
@@ -16,14 +35,23 @@ class clean_data:
         clean_data=''
         clean_data=clean_data.join(raw_data)
         return clean_data
-
+    
+""" CDS Cleaning """
 
     def clean_cds_region(raw_data):
+        """ Cleaning
+        input: raw_data clean data
+        output: raw_data raw data
+        """
         raw_data=str(raw_data)
         clean_data=re.sub('[join()<>+{}]','', raw_data)
         return clean_data
 
     def clean_cds_loc(raw_data):
+        """ Cleaning
+        input: raw_data clean data 
+        output: raw_data i removed
+        """
         dirt = ('j','o','i','n','{','}','(',')','+','[',']')
         for i in raw_data:
             if i == dirt:
@@ -31,11 +59,14 @@ class clean_data:
         print(raw_data)
 
     def remove_apost(raw_data):
-
+         """ Cleaning
+        input: raw_data clean data 
+        output: raw_data space removed
+        """
         clean_data = raw_data.replace('"','')
         return clean_data
 
-
+""" Remove Version Nunmber in Acession_No """
     def remove_version(acc_no):
         '''Accession numbers captured have a version number after a decimal point
         at the end of the number i.e AB12345678.1 This removes the version number [.1]
@@ -48,7 +79,7 @@ class clean_data:
                 acc_no = acc_no[:-2] #captures all except the last two places
         return acc_no
 
-
+""" White space removal """"
     def clean_wspace(dna_seq):
         '''Remove white space and line seperators in all forms
         input: raw DNA sequence
@@ -58,7 +89,7 @@ class clean_data:
         dna_seq = dna_seq.replace(' ', '')
         return(dna_seq)
 
-
+""" Line Removal """"
     def clean_lines(raw_data):
         '''Remove line seperators in all forms
         input: raw DNA sequence
@@ -67,7 +98,7 @@ class clean_data:
         raw_data = raw_data.replace('\n', '')
         return(raw_data)
 
-
+"""Removing Number in DNA sequence """
 
     def clean_nos(raw_data):
         '''Remove all numbers from FASTA format DNA sequence
@@ -79,6 +110,8 @@ class clean_data:
 
         return(dna_seq)
 
+""" Capitalise All"""
+    
     def capitalise(raw_data):
         '''Entire string is capitalised
         input: single string in upper and lowercase forms
@@ -89,7 +122,7 @@ class clean_data:
             dna_seq=raw_data.upper()
         return dna_seq
 
-    
+ """ Ignore N's"""
 
     def ignore_nnn(raw_data):
         '''ignore database codon entries with N
