5cc8b5636b89171f8cce3f5e1d1b9e0da6f8ee73
mmarto27 <48446427+mmarto27@users.noreply.github.com>
2019-05-05 21:29:47 +0100

    Update configFile.py

--- business_tier/configFile.py
+++ business_tier/configFile.py
@@ -8,6 +8,17 @@ dbArg5 = 'TRANSLATION'
 dbArg6 = 'CDS_REGIONS'
 dbArg7 = 'DNA_SEQUENCE'
 
-
 cds_start_char = '<'
 cds_end_char  = '>'
+
+ecoRI_forward = 'GAATTC'
+ecoRI_reverse = 'CTTAAG'
+ecoRI_char = '@'
+
+bamHI_forward = 'GGATCC'
+bamHI_reverse = 'CCTAGG'
+bamHI_char = '$'
+
+bsuMI_forward = 'CTCGAG'
+bsuMI_reverse = 'GAGCTC'
+bsuMI_char = '%'
