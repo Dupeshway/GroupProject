9fb95253ec8efa9848c0beae89c8393d8fa030c3
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-05 10:08:49 +0100

    Update front_end_test.py

--- test_suite/front_end_test.py
+++ test_suite/front_end_test.py
@@ -22,29 +22,29 @@ from selenium.webdriver.common.keys import Keys
 """ Front End Test Suite"""
 class FronTendSearch(unittest.TestCase):
 
-Gene_id_code = ""
-Products_protein = ""
+Gene_id = ""
+Protein_product_names = ""
 Accession_number = ""
-Seq_Locaton = ""
+Chromosomal location = ""
 
     def setUp(self):
         """ Open web page in firefox."""
         self.driver = webdriver.Firefox()
     def test_front_end(self):
-        """Website Testing.
+        """ Web testing for fron end
             Input: Url for fornt end
             Ouput Search pass or not"""
         driver = self.driver
-        driver.get(" Need URL from front end ")
+        driver.get(" http://student.cryst.bbk.ac.uk/cgi-bin/cgiwrap/wc005/cgi_script.py")
         self.assertIn("Gene_Header ", driver.title)
-        elem = driver.find_element_by_id("Gene_id")
+        elem = driver.find_element_by_id("Gene")
         elem.send_keys(Gene_id_code)
-        elem = driver.find_element_by_id("Products")
-        elem.send_keys(Products_protein)
+        elem = driver.find_element_by_id("Protein")
+        elem.send_keys(Protein product names)
         elem = driver.find_element_by_id("Accession")
         elem.send_keys(Accession_number)
-        elem = driver.find_element_by_id("Seq_Locaton")
-        elem.send_keys(Seq_Locaton)
+        elem = driver.find_element_by_id("Chromosomal")
+        elem.send_keys(Chromosomal location )
         elem.send_keys(Keys.RETURN)
         assert "No results found." not in driver.page_source
 
