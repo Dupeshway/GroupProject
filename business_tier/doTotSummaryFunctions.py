3d6bc15518ea75f1a0e192d40f6e7b27d292a26c
mmarto27 <48446427+mmarto27@users.noreply.github.com>
2019-05-06 23:12:47 +0100

    Update and rename doSummaryFunctions.py to doTotSummaryFunctions.py

--- business_tier/doTotSummaryFunctions.py
+++ business_tier/doTotSummaryFunctions.py
@@ -2,32 +2,32 @@
 from SQL_python_functions import db_summary
 from configFile import *
 
-class doSummaryFunctions:
+class doTotSummaryFunctions:
 
-     def getAccessionNumber():
+     def getTotAccessionNumber():
           accession_number = db_summary(dbArg1)
           return accession_number
 
-     def getChrLocation():
+     def getTotChrLocation():
           chr_location = db_summary(dbArg2)
           return chr_location
 
-     def getGeneID():
+     def getTotGeneID():
           gene_id = db_summary(dbArg3)
           return gene_id
 
-     def getProductName():
+     def getTotProductName():
           product_name = db_summary(dbArg4)
           return product_name
 
-     def getAminoAcidSequence():
+     def getTotAminoAcidSequence():
           amino_acid_sequence = db_summary(dbArg5)
           return amino_acid_sequence
 
-     def getLocationCDS():
+     def getTotLocationCDS():
           cds_location = db_summary(dbArg6)
           return cds_location
 
-     def getDNA():
+     def getTotDNA():
           dna_sequence = db_summary(dbArg7)
           return dna_sequence
