873686467c0edb92f2e3ed08a9097e0bd4326bce
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-06 06:56:27 -0400

    Update doEndCDS.py

--- business_tier/doEndCDS.py
+++ business_tier/doEndCDS.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 #from database_layer import db_query
+"""
+Program:Start CDS
+File:doStartCDS.py
+Version:1.0
+Date:May-5-2019
+Author: Margherita Martorana, Documentation headers(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description: Funciton to Process Coordine ends from the CDS location
+--------------------------------------------------------------------------
+Import libraries:
+import re
+import getLocationCDS
+"""
 import re
 import getLocationCDS
 
 def getEndCDS(input_value, input_value):
+     """ Process Coordine ends from the CDS location 
+     Input: input type and vaule in cofig file
+     Output:codinate end of CDS location """
      x = getLocationCDS(input_value, input_value)
      coordinate_end = re.findall(r'\-(\d+)', x)
 
