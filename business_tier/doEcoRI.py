3c19228a3568d62f1b6535919baff6ed5890fa4d
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-06 07:09:50 -0400

    Update doEcoRI.py

--- business_tier/doEcoRI.py
+++ business_tier/doEcoRI.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 #from database_layer import db_query
+
+"""
+Program:Start CDS
+File:doStartCDS.py
+Version:1.0
+Date:May-5-2019
+Author: Margherita Martorana, Documentation headers(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description: Funciton to process DNA sequence with restriciton enzyme ecoRI
+--------------------------------------------------------------------------
+from configFile import *
+import re
+"""
+
 from configFile import *
 import re
 
 def doEcoRI(seq):
+     """ Process Sequence with restriction enzyme EcoRi
+     Input: input ecoRI from cofigfile 
+     Output:Sequence """
      f = re.compile(r'' + ecoRI_forward + '')
      r = re.compile(r'' + ecoRI_reverse + '')
      seq = f.sub(ecoRI_forward[0] + ecoRI_char + \
