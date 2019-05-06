cbbcfa391632797a000a930cc1dbba6b7d1ec6aa
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-05 21:23:02 -0400

    Update getGeneID.py

--- business_tier/getGeneID.py
+++ business_tier/getGeneID.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 #from database_layer import db_query
+"""
+Program:Obtain Chromosome Location
+File:getLocationCDS.py
+Version:1.0
+Date:May-4-2019
+Author: Margherita Martorana, Documentation headers(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description: Funciton to getting Chromosome Location from the database
+--------------------------------------------------------------------------
+Import libraries: import getDataFromDB
+"""
+
 import getDataFromDB
 
 def getGeneID(input_type, input_value):
+      """Getting Gene ID Function from Database
+     Input: input type and vaule in cofig 
+     Output:Gene ID
+     """
      gene_id = getDataFromDB(dbArg3, input_type, input_value)
      return gene_id
