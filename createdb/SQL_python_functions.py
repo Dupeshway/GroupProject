fbd495f9beda096d647ae2a2d5e27a7e35dd1f78
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-06 19:34:16 -0400

    Update SQL_python_functions.py

--- createdb/SQL_python_functions.py
+++ createdb/SQL_python_functions.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python3
+
+
+"""
+Program:Cleaning Data
+File:cleaning_data.py
+Version:1.0
+Date:May-4-2019
+Author: Yobi Livingstone, Documentation headers(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description: Functions to clean raw data 
+--------------------------------------------------------------------------
+Usage: For processing data to the bussiness layer to make it consistent
+and easier to handle.
+--------------------------------------------------------------------------
+Import libraries: import pymysql.cursors
+"""
+
 import pymysql.cursors
+
+""" SQL Query Module"""
 
 class sql_query:
 
@@ -10,6 +30,7 @@ class sql_query:
         port     = 3306
         db = pymysql.connect(host=dbhost, port=port, user=dbuser, passwd=dbpass, db=dbname)
         
+        """ Data Base Query Function"""
         def db_query(output_type, input_type, input_value):
                 """Retreive search query when given query parameters;
                 output_type, input_type and input_value, provided by the webuser
@@ -25,7 +46,7 @@ class sql_query:
                 return output_value
 
 
-
+        """ Data Base Summary Function"""
         def db_summary(output_type):
                 '''select (*)
                 Returning entire record assoiciated with Accession no.
