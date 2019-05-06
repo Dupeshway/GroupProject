617f74a89b4244a1dcdcc4d09b5d175b6f30186b
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-05 21:17:20 -0400

    Update getLocationCDS.py

--- business_tier/getLocationCDS.py
+++ business_tier/getLocationCDS.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 #from database_layer import db_query
+"""
+Program:Obtain Chromosome Location
+File:getLocationCDS.py
+Version:1.0
+Date:May-4-2019
+Author: Margherita Martorana, Documentation headers(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description: Funciton to getting Chromosome Location from the database
+--------------------------------------------------------------------------
+Import libraries: import getDataFromDB
+"""
 import getDataFromDB
 
 def getLocationCDS(input_value, input_value):
+          """Getting Protein Product Name Function
+     Input: input type and vaule in cofig 
+     Output:Protein Chromosome Location
+     """
      cds_location = getDataFromDB(dbArg6, input_value, output_value)
      return cds_location
 
