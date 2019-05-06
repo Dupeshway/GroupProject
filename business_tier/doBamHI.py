7f77a61cd905374fe47c64134b7623adea01cf10
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-06 07:22:33 -0400

    Update doBamHI.py

--- business_tier/doBamHI.py
+++ business_tier/doBamHI.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 #from database_layer import db_query
+"""
+Program:Restriction Enzyme with BamHI
+File:doBamHI.py
+Version:1.0
+Date:May-5-2019
+Author: Margherita Martorana, Documentation headers(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description: Funciton to process DNA sequence with restriciton enzyme BamHI
+--------------------------------------------------------------------------
+from configFile import *
+import re
+"""
+
 from configFile import *
 import re
 
 def doBamHI(seq):
+     """ Process Sequence with restriction enzyme BamHI
+     Input: input ecoRI from cofigfile 
+     Output:DNA Sequence """
      f = re.compile(r'' + bamHI_forward + '')
      r = re.compile(r'' + bamHI_reverse + '')
      seq = f.sub(bamHI_forward[0] + bamHI_char + \
