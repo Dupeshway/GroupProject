4ca82dd73fb3eb12a7a3df46b424204521fef13f
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-05 22:55:30 -0400

    Update doRestrictionEnzymes.py

--- business_tier/doRestrictionEnzymes.py
+++ business_tier/doRestrictionEnzymes.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 #from database_layer import db_query
+"""
+Program:Start CDS
+File:doStartCDS.py
+Version:1.0
+Date:May-5-2019
+Author: Margherita Martorana, Documentation headers(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description: Funciton to process DNA sequence incorporating restriciton enzyme
+--------------------------------------------------------------------------
+Import libraries:
+import getDNA
+import doEcoRI
+import doBamHI
+import doBsuMI
+import re
+"""
 import getDNA
 import doEcoRI
 import doBamHI
@@ -7,6 +24,9 @@ import doBsuMI
 import re
 
 def doRestrictionEnzymes(input_type, input_value):
+     """ Process DNA function with restriction enzyme
+     Input: input type and vaule in cofig 
+     Output:CDS start Coordinate """
      x = getDNA(input_type, input_value)
      y = doEcoRI(x)
      z = doBamHI(y)
