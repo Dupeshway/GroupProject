39bb66dd4a2284d5c81c891c9116fa58134b773b
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-06 07:21:14 -0400

    Update doBsuMI.py

--- business_tier/doBsuMI.py
+++ business_tier/doBsuMI.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 #from database_layer import db_query
+
+"""
+Program:Restriction Enzyme with BsuMI
+File:doBsuMI.py
+Version:1.0
+Date:May-5-2019
+Author: Margherita Martorana, Documentation headers(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description: Funciton to process DNA sequence with restriciton enzyme BsuMI
+--------------------------------------------------------------------------
+from configFile import *
+import re
+"""
+
 from configFile import *
 import re
 
 def doBsuMI(seq):
+     """ Process Sequence with restriction enzyme EcoRi
+     Input: input ecoRI from cofigfile 
+     Output:Sequence """
      f = re.compile(r'' + bsuMI_forward + '')
      r = re.compile(r'' + bsuMI_reverse + '')
      seq = f.sub(bsuMI_forward[0] + bsuMI_char + \
