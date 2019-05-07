4c5dd38773f80e2ec86771e3d902a4d1f7f1bcb5
mmarto27 <48446427+mmarto27@users.noreply.github.com>
2019-05-07 14:39:07 +0100

    Update doTotSummaryFunctions.py

--- business_tier/doTotSummaryFunctions.py
+++ business_tier/doTotSummaryFunctions.py
@@ -18,37 +18,38 @@ from SQL_python_functions import db_summary
 from configFile import *
 
 """Summary Functions Module"""
-class doTotSummaryFunctions:
 
-     def getTotAccessionNumber():
-          """Full summary of Accession Number"""
-          accession_number = db_summary(dbArg1)
-          return accession_number
+def getTotAccessionNumber():
+     """Full summary of Accession Number"""
+     accession_number = db_summary(dbArg1)
+     return accession_number
 
-     def getTotChrLocation():
-          """Full summary of chromosome location"""
-          chr_location = db_summary(dbArg2)
-          return chr_location
+def getTotChrLocation():
+     """Full summary of chromosome location"""
+     chr_location = db_summary(dbArg2)
+     return chr_location
   
-     def getTotGeneID():
-          """Full summary of Gene ID"""
-          gene_id = db_summary(dbArg3)
-          return gene_id
-    
-     def getTotProductName():
-          """ Full Summary of Protein Product Name"""
-          product_name = db_summary(dbArg4)
-          return product_name
+def getTotGeneID():
+     """Full summary of Gene ID"""
+     gene_id = db_summary(dbArg3)
+     return gene_id
+     
+def getTotProductName():
+     """ Full Summary of Protein Product Name"""
+     product_name = db_summary(dbArg4)
+     return product_name
 
-     def getTotAminoAcidSequence():
-          """ Full Summary of Amino Acid Sequence"""
-          amino_acid_sequence = db_summary(dbArg5)
-          return amino_acid_sequence
-     def getTotLocationCDS():
-          """ Full Summary of Location of CDS"""
-          cds_location = db_summary(dbArg6)
-          return cds_location
-     def getTotDNA():
-          """ Full Summary of DNA"""
-          dna_sequence = db_summary(dbArg7)
-          return dna_sequence
+def getTotAminoAcidSequence():
+     """ Full Summary of Amino Acid Sequence"""
+     amino_acid_sequence = db_summary(dbArg5)
+     return amino_acid_sequence
+
+def getTotLocationCDS():
+     """ Full Summary of Location of CDS"""
+     cds_location = db_summary(dbArg6)
+     return cds_location
+
+def getTotDNA():
+     """ Full Summary of DNA"""
+     dna_sequence = db_summary(dbArg7)
+     return dna_sequence
