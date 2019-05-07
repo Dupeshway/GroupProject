81bf485ccfda493f33d9abcae3c91f5550c9f6d7
Yobi Livingstone <39736534+Dupeshway@users.noreply.github.com>
2019-05-07 15:14:17 +0100

    Add files via upload

--- cgi-biocomp2/config.py
+++ cgi-biocomp2/config.py
@@ -1,1 +1,30 @@
+#!/usr/bin/env python3
 
+#Yobi Livingstone
+
+class config:
+
+    '''To store all variables to be called across modules'''
+    
+    test_file = 'test_chrom8.txt'
+    r_file = 'chrom_CDS_8.txt'
+
+    acc_file = 'parsed_acc_no.txt'
+    sql_acc_file = 'sql_acc_no.sql'
+
+
+    prod_file= 'sql_product_name.sql'
+
+    dna_file = 'parsed_dna_seq.txt'
+    acc_dna_file = 'parsed acc_dna_seq.txt'
+    sql_dna_file= 'sql_dna_seq.sql'
+
+    cds_file = 'parsed_cds_regions.txt'
+    sql_cds_file = 'sql_cds_regions.sql'
+
+    trans_file = 'parsed_translation.txt'
+    sql_trans_file= 'sql_translation.sql'
+
+    gene_file = 'sql_gene_id.sql'
+
+    chrom_file = 'sql_chrom_loc.sql'
