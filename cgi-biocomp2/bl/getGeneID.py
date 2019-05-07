7c644a8fa5940dac2aed1c24b2b30baccac527f8
mmarto27 <48446427+mmarto27@users.noreply.github.com>
2019-05-07 17:32:43 +0100

    Update getGeneID.py

--- cgi-biocomp2/bl/getGeneID.py
+++ cgi-biocomp2/bl/getGeneID.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-from SQL_python_functions import db_query
-from configFile import *
-
 """
 Program:Obtain GeneID
 File:getGeneID.py
@@ -13,16 +10,24 @@ Address: Biological Sciences, Birkbeck ...
 Description: Function for getting GeneID from the database 
 --------------------------------------------------------------------------
 imported Libraries:
-from SQL_python_functions import *
-from configFile import *
+from SQL_python_functions import db_query
+from config import config as cg
+import sys
 """
+
+from SQL_python_functions import db_query
+from config import config as cg
+import sys
+
+sys.path.insert(0, '../db/')
+sys.path.insert(0, '../')
 
 """ Getting Gene ID"""
 def getGeneID(input_type, input_value):
      """ Function to obtain protein GeneID  from the database
      input: database query Arg3, type, and value from the config file
      output:Gene ID from database"""
-     gene_id = db_query(dbArg3, input_type, input_value)
+     gene_id = db_query(cg.dbArg3, input_type, input_value)
      if gene_id == '':
           return 'Gene ID missing'
      else:
