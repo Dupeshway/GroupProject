7fa53633045a9dce1c49ace6f564a75e07a6d9a1
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-06 21:19:55 -0400

    Update getAccessionNumber.py

--- business_tier/getAccessionNumber.py
+++ business_tier/getAccessionNumber.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
+"""
+Program:Accession Number
+File:getAccessionNumber.py
+Version:1.0
+Date:May-5-2019
+Author: Margherita Martorana, Documentation headers(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description: Function for getting Accession Number from the database 
+--------------------------------------------------------------------------
+imported Libraries:
+from SQL_python_functions import *
+from configFile import *
+"""
 from SQL_python_functions import db_query
 from configFile import *
 
+""" Getting Accession Number"""
 def getAccessionNumber(input_type, input_value):
+     """ Function to obtain accession number from the database
+     input: database query with Arg4, type and value from the config file
+     output: accession numberfrom database """
      accession_number = db_query(dbArg1, input_type, input_value)
 
      return accession_number
