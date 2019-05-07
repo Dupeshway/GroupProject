7bb1b82597d42597ede1fd3ed11f16ccfeec3625
mmarto27 <48446427+mmarto27@users.noreply.github.com>
2019-05-07 14:32:48 +0100

    Update SQL_python_functions.py

--- createdb/SQL_python_functions.py
+++ createdb/SQL_python_functions.py
@@ -15,13 +15,36 @@ Usage: For Database Query and setting up database summary
 --------------------------------------------------------------------------
 Import libraries: import pymysql.cursors
 """
-
 import pymysql.cursors
+        
+""" Data Base Query Function"""
+def db_query(output_type, input_type, input_value):
+        """Retreive search query when given query parameters;
+        output_type, input_type and input_value, provided by the webuser
+        input= output_type, input_type, input_value
+        query parameters for data retrieval
+        output= output_value within the provided parameters
+        """
+        
+        dbname   = "ly001"
+        dbhost   = "ssh.cryst.bbk.ac.uk" #Replace ssh with hope when working from nomachine
+        dbuser   = "ly001"   # Ask a demonstrator
+        dbpass   = "7#b8tpkum"   # Ask a demonstrator
+        port     = 3306
+        db = pymysql.connect(host=dbhost, port=port, user=dbuser, passwd=dbpass, db=dbname)
+                
+        sql = "select "+output_type+" from CHROM8 where "+input_type+"='"+input_value+"'"
+        cursor = db.cursor()
+        data  = cursor.execute(sql)
+        output_value   = cursor.fetchall()
+        return output_value
 
-""" SQL Query Module"""
 
-class sql_query:
-
+""" Data Base Summary Function"""
+def db_summary(output_type):
+        '''select (*)
+        Returning entire record assoiciated with Accession no.
+        '''
         dbname   = "ly001"
         dbhost   = "ssh.cryst.bbk.ac.uk" #Replace ssh with hope when working from nomachine
         dbuser   = "ly001"   # Ask a demonstrator
@@ -29,29 +52,8 @@ class sql_query:
         port     = 3306
         db = pymysql.connect(host=dbhost, port=port, user=dbuser, passwd=dbpass, db=dbname)
         
-        """ Data Base Query Function"""
-        def db_query(output_type, input_type, input_value):
-                """Retreive search query when given query parameters;
-                output_type, input_type and input_value, provided by the webuser
-                input= output_type, input_type, input_value
-                query parameters for data retrieval
-                output= output_value within the provided parameters
-                """
-
-                sql = "select "+output_type+" from CHROM8 where "+input_type+"='"+input_value+"'"
-                cursor = db.cursor()
-                data  = cursor.execute(sql)
-                output_value   = cursor.fetchall()
-                return output_value
-
-
-        """ Data Base Summary Function"""
-        def db_summary(output_type):
-                '''select (*)
-                Returning entire record assoiciated with Accession no.
-                '''
-                sql="select "+output_type+" from CHROM8"
-                cursor = db.cursor()
-                data  = cursor.execute(sql)
-                output_value   = cursor.fetchall()
-                return output_value
+        sql="select "+output_type+" from CHROM8"
+        cursor = db.cursor()
+        data  = cursor.execute(sql)
+        output_value   = cursor.fetchall()
+        return output_value
