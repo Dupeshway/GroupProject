5f288b22e9261ad4923a49c986cdd048886d158b
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-06 22:33:35 -0400

    Update front_end_test.py

--- test_suite/front_end_test.py
+++ test_suite/front_end_test.py
@@ -43,10 +43,71 @@ Chromosomal location = ""
         self.assertIn("Gene Database Project ", driver.title)
         elem = driver.find_element_by_id("Gene Indentifier")
         elem.send_keys(Gene_id)
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+def tearDown(self):
+    """Closing browser Window"""
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
+
+    
+     def setUp(self):
+        """ Open web page in firefox."""
+        self.driver = webdriver.Firefox()
+    def test_front_end(self):
+        """ Web testing for fron end
+            Input: Url for fornt end
+            Ouput Search pass or not"""
+        driver = self.driver
+        driver.get(" http://student.cryst.bbk.ac.uk/cgi-bin/cgiwrap/wc005/cgi_script.py")
+        self.assertIn("Gene Database Project ", driver.title)
         elem = driver.find_element_by_id("Protein product names")
         elem.send_keys(Protein_product)
-        elem = driver.find_element_by_id(" GeneBank accession number")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+def tearDown(self):
+    """Closing browser Window"""
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
+    
+     def setUp(self):
+        """ Open web page in firefox."""
+        self.driver = webdriver.Firefox()
+    def test_front_end(self):
+        """ Web testing for fron end
+            Input: Url for fornt end
+            Ouput Search pass or not"""
+        driver = self.driver
+        driver.get(" http://student.cryst.bbk.ac.uk/cgi-bin/cgiwrap/wc005/cgi_script.py")
+        self.assertIn("Gene Database Project ", driver.title)
+        elem = driver.find_element_by_id("Gene Indentifier")
         elem.send_keys(Accession_number)
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+def tearDown(self):
+    """Closing browser Window"""
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
+
+ def setUp(self):
+        """ Open web page in firefox."""
+        self.driver = webdriver.Firefox()
+    def test_front_end(self):
+        """ Web testing for fron end
+            Input: Url for fornt end
+            Ouput Search pass or not"""
+        driver = self.driver
+        driver.get(" http://student.cryst.bbk.ac.uk/cgi-bin/cgiwrap/wc005/cgi_script.py")
+        self.assertIn("Gene Database Project ", driver.title)
         elem = driver.find_element_by_id("Chromosomal location ")
         elem.send_keys(Chromosomal location )
         elem.send_keys(Keys.RETURN)
