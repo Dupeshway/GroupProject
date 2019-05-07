d31baf65621572419be5584d507c5773c2a2e441
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-06 23:24:56 -0400

    Update doCodonDictionary.py

--- business_tier/doCodonDictionary.py
+++ business_tier/doCodonDictionary.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
+"""
+Program:Dictionary for Condon
+File:doCodonDictionary.py
+Version:1.0
+Date:May-7-2019
+Author: Margherita Martorana, Documentation headers(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description: Dictionary for Codeon of the Sequenc  
+--------------------------------------------------------------------------
+from SQL_python_functions import *
+import doCodons
+from configFile import *
+"""
 from SQL_python_functions import *
 import doCodons
 from configFile import *
 
+""" Condon Dictionary"""
 def doCodonDictionary():
+     """ Functions for a dicitonary for DNA sequence codon
+     input: dna sequence
+     output: DNA sequnce with the codon"""
      d = {}
      dna_sequence = db_summary(dbArg7)
      codon = doCodons(dna_sequence)
