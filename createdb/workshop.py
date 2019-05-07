9ed761f9c17e09b0f8d342826fdcf54cd43df874
Yobi Livingstone <39736534+Dupeshway@users.noreply.github.com>
2019-05-07 15:31:33 +0100

    Update workshop.py

--- createdb/workshop.py
+++ createdb/workshop.py
@@ -3,12 +3,12 @@
 #Yobi Livingstone
 
 
-#Building SUITE
-'''only for creating and testing functions'''
+#WORKSHOP AND TESTING SUITE
+'''DO NOT RUN THESE LEGACY FUNCTIONS AS THEY MAY INTERFERE WITH PARSING SUITE'''
 
 import re
 
-from config_db import config as cg
+from config import config as cg
 from file_management import file_management as fm
 from cleaning_data import clean_data as cl
 from SQL_format import sql_format as sf
@@ -16,8 +16,7 @@ from SQL_format import sql_format as sf
 
 class workshop:
 
-
-
+"""
     def sql_parse_cds(datafile):
         '''This function converts the output of parse_cds(),
         input: text file with accession number and protein trnaslation in alternating lines
@@ -48,7 +47,6 @@ class workshop:
 
 fm.wipe_file(cg.sql_cds_file)
 fm.write_file(workshop.sql_parse_cds(cg.cds_file), cg.sql_cds_file)
-
 
 
 
@@ -211,7 +209,9 @@ fm.write_file(testing.sql_parse_prot_trans(cg.trans_file), cg.sql_trans_file)
 fm.wipe_file(cg.trans_file)
 fm.write_file(testing.parse_prot_trans(cg.r_file), cg.trans_file)
 
- 
+
+
+
 
     def parse_product_name(datafile):
         '''Captures the protein product name from a Genbank file
@@ -492,8 +492,96 @@ TESTING FOR GENE ID
 
 
 -------------------------------------------------------------------------------------
-#TESTING FOR ACC and DNA SEQ:
+#TESTING FOR ACC, GENE_ID AND DNA SEQ:
 
+
+    def v2parse_gene_id(datafile):
+        '''Capture one copy of feature in each record of genbank file
+        input: Genbank record
+        output: seperate strings for each record
+        '''
+        
+        p=re.compile(r'//')
+        pgene=re.compile(r'gene="[A-Z]') 
+        gene_id=''
+
+        gene_counter=0
+        count=0
+        datafile=fm.open_file(datafile)
+
+        for i in datafile:
+            if p.findall(i): #finds // 
+                count=0
+                
+
+            elif count==0:
+                    if pgene.findall(i):
+                        gene_id+= i[28:33]+'\n' #finds gene="[A-Z] 
+                        count+=1
+
+        return cl.remove_apost(gene_id) #removing apostraphes and whitespaces           
+
+
+    def v1parse_gene_id(datafile):
+        '''This function captures lines the the gene_id
+        input: strings of each line in text file
+        ouput: 
+        '''
+        p=re.compile(r'gene="[A-Z]') 
+        #there are between 6-8 letters/number in an acc_no
+
+        gene_id=''
+        raw_data=fm.open_file(datafile)
+        for i in raw_data:
+            if p.findall(i):
+                gene_id += i[28:33]+'\n'
+
+        return(cl.remove_apost(gene_id))
+        return(cl.clean_wspace(gene_id))
+
+
+
+    def parse_acc_no(datafile):
+        '''This function is to capture lines that begin
+        with the string: Accession
+        input: strings of each line in text file
+        ouput: return captured accession numbers
+        '''
+        p=re.compile(r'ACCESSION\s+\w+') 
+        #there are between 6-8 letters/number in an acc_no
+
+        acc_no=''
+
+        for i in datafile:
+            if p.findall(i):
+                acc_no += i[:20]
+
+        return acc_no
+
+
+
+    def parse_dna_seq(datafile):
+
+        p=re.compile(r'ORIGIN *')
+        dna_seq=''
+        
+        for i in datafile:
+            if p.findall(i):
+                dna_seq += i
+            
+        return dna_seq
+
+
+    def clean_cds_region(raw_data):
+        dirt = ('j','o','i','n','{','}','(',')','+','[',']')
+        for i in raw_data:
+            if i == dirt:
+                raw_data=raw_data.replace(i,'')
+        print(raw_data)
+
+
+
+print(legacy.parse_acc_no(cg.r_file))
 
 
 
