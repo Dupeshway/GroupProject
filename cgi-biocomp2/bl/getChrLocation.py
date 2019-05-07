047645b9be1ef2c2019837c36f017c971510e03d
mmarto27 <48446427+mmarto27@users.noreply.github.com>
2019-05-07 17:29:34 +0100

    Update getChrLocation.py

--- cgi-biocomp2/bl/getChrLocation.py
+++ cgi-biocomp2/bl/getChrLocation.py
@@ -10,17 +10,23 @@ Address: Biological Sciences, Birkbeck ...
 Description: Function for getting Chromosome Location from the database 
 --------------------------------------------------------------------------
 imported Libraries:
-from SQL_python_functions import *
-from configFile import *
+from SQL_python_functions import db_query
+from config import config as cg
+import sys
 """
 from SQL_python_functions import db_query
-from configFile import *
+from config import config as cg
+import sys
+
+sys.path.insert(0, '../db/')
+sys.path.insert(0, '../')
+
 """ Getting Chomosome Locaiton"""
 def getChrLocation(input_type, input_value):
      """ Function to obtain Chromosome Location from the database
      input: database query Arg2, type, and value from the config file
      output:Chromosome Location from database"""
-     chr_location = db_query(dbArg2, input_type, input_value)
+     chr_location = db_query(cg.dbArg2, input_type, input_value)
      if chr_location = '':
           return 'Chromosomal Location Missing'
      else:
