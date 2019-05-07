2a1c5de7c1f4114e0436953ed2de5d9a045c463e
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-06 20:54:09 -0400

    Update getProductName.py

--- business_tier/getProductName.py
+++ business_tier/getProductName.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
+
+"""
+Program:Processing codon dicitonary
+File:getProductName.py
+Version:1.0
+Date:May-5-2019
+Author: Margherita Martorana, Documentation headers(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description: Function for getting Protein Product Name from the database 
+--------------------------------------------------------------------------
+imported Libraries:
+from SQL_python_functions import *
+import doCodons
+from configFile import *
+"""
 from SQL_python_functions import db_query
 from configFile import *
 
+""" Obtaingint product Name"""
+
 def getProductName(input_type, input_value):
+     """ Function to obtain protein product name from the database
+     input: database query with Arg4, type and value from the config file
+     output:protein product name from database """
+     
      product_name = db_query(dbArg4, input_type, input_value)
 
      return product_name
