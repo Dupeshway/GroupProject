c000ccfba4a93f1b2ff0196812c624ad1bb769b0
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-07 05:28:19 -0400

    Update doTotCodonUsage.py

--- business_tier/doTotCodonUsage.py
+++ business_tier/doTotCodonUsage.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
+"""
+Program:Tot Condon Usage
+File:doTotCodonUsage.py
+Version:1.0
+Date:May-7-2019
+Author: Margherita Martorana, Documentation headers(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description: Function to process DNA sequence 
+--------------------------------------------------------------------------
+from SQL_python_functions import *
+import re
+import doCodonDictionary
+from configFile import *
+"""
 from SQL_python_functions import *
 import doCodonDictionary
 import doCodons
 from configFile import *
 
 def doTotCodonUsage():
+     def doCodons(dna_sequence):
+     """ Functions to process DNA sequence Tot codon usage
+     input: dna sequence
+     output: count of codon"""
      d = doCodonDictionary()
      dna_sequence = db_summary(dbArg7)
      codon_count = 0
