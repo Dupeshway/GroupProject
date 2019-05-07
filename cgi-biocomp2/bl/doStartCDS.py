c4ef76011829d7028c503165e7d8c537111e68eb
mmarto27 <48446427+mmarto27@users.noreply.github.com>
2019-05-07 20:55:11 +0100

    Update doStartCDS.py

--- cgi-biocomp2/bl/doStartCDS.py
+++ cgi-biocomp2/bl/doStartCDS.py
@@ -19,7 +19,7 @@ import getLocationCDS
 import re
 import getLocationCDS
 
-""" Get start CDS"""
+#""" Get start CDS"""
 def getStartCDS(input_value, input_value):
       """ Process CDS from the getLocationCDS
      Input: input type and value in config 
