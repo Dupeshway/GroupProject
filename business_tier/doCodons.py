6acfc387db2ec05eb23c4e442d79247b747a4b9b
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-06 23:17:45 -0400

    Update doCodons.py

--- business_tier/doCodons.py
+++ business_tier/doCodons.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
+"""
+Program:Restriction Enzyme with BamHI
+File:doBamHI.py
+Version:1.0
+Date:May-7-2019
+Author: Margherita Martorana, Documentation headers(Jeff Li)
+Address: Biological Sciences, Birkbeck ...
+--------------------------------------------------------------------------
+Description: Function to process DNA sequence 
+--------------------------------------------------------------------------
+from SQL_python_functions import *
+import re
+import doCodonDictionary
+from configFile import *
+"""
 from SQL_python_functions import *
 import re
 import doCodonDictionary
 from configFile import *
 
+""" Process codon of DNA Sequence""" 
 def doCodons(dna_sequence):
+     """ Functions to process DNA sequence codon
+     input: dna sequence
+     output: DNA sequnce with the codon"""
      p = re.compile(r'[ATCG]{3}')
      dna_sequence = db_query(dbArg7, input_type, input_value)
 
