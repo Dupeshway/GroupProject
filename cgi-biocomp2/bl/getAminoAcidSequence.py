fbef3b22e98a488648aaf6ca7e3ce400885d044f
mmarto27 <48446427+mmarto27@users.noreply.github.com>
2019-05-07 17:28:20 +0100

    Update getAminoAcidSequence.py

--- cgi-biocomp2/bl/getAminoAcidSequence.py
+++ cgi-biocomp2/bl/getAminoAcidSequence.py
@@ -10,17 +10,22 @@ Address: Biological Sciences, Birkbeck ...
 Description: Function for getting amino acid sequence from the database 
 --------------------------------------------------------------------------
 imported Libraries:
-from SQL_python_functions import *
-from configFile import *
+from SQL_python_functions import db_query
+from config import config as cg
+import sys
 """
 from SQL_python_functions import db_query
-from configFile import *
+from config import config as cg
+import sys
+
+sys.path.insert(0, '../db/')
+sys.path.insert(0, '../')
 
 """Get AMino Scid Sequence"""
 def getAminoAcidSequence(input_type, input_value):
      """ Function to obtain Amino Acid Sequence  from the database
      input: database query Arg5, type, and value from the config file
      output:Amino Acid Sequence from database"""
-     amino_acid_sequence = db_query(dbArg5, input_type, input_value)
+     amino_acid_sequence = db_query(cg.dbArg5, input_type, input_value)
      
      return amino_acid_sequence
