d546896e8dea04016b81ca285c3d3e72de7da140
jeffrey999000 <48446446+jeffrey999000@users.noreply.github.com>
2019-05-07 05:36:41 -0400

    Update doTotSummaryFunctions.py

--- business_tier/doTotSummaryFunctions.py
+++ business_tier/doTotSummaryFunctions.py
@@ -20,31 +20,35 @@ from configFile import *
 """Summary Functions Module"""
 class doTotSummaryFunctions:
 
-     """ Summary of Accssion Number"""
      def getTotAccessionNumber():
+          """Full summary of Accession Number"""
           accession_number = db_summary(dbArg1)
           return accession_number
-      """ Summary of Chromosome Location"""
+
      def getTotChrLocation():
+          """Full summary of chromosome location"""
           chr_location = db_summary(dbArg2)
           return chr_location
-     """ Summary of Gene Id """
+  
      def getTotGeneID():
+          """Full summary of Gene ID"""
           gene_id = db_summary(dbArg3)
           return gene_id
-     """ Summary of Product Name"""
+    
      def getTotProductName():
+          """ Full Summary of Protein Product Name"""
           product_name = db_summary(dbArg4)
           return product_name
-     """ Summary of Amino Acid Sequence"""
+
      def getTotAminoAcidSequence():
+          """ Full Summary of Amino Acid Sequence"""
           amino_acid_sequence = db_summary(dbArg5)
           return amino_acid_sequence
-     """ Summary of Location of CDS"""
      def getTotLocationCDS():
+          """ Full Summary of Location of CDS"""
           cds_location = db_summary(dbArg6)
           return cds_location
-     """ Summary of Location of DNA"""
      def getTotDNA():
+          """ Full Summary of DNA"""
           dna_sequence = db_summary(dbArg7)
           return dna_sequence
