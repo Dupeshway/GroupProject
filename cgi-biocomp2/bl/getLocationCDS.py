6f8029ce7f116116ee686dc7f8d8be3fc07ebe27
mmarto27 <48446427+mmarto27@users.noreply.github.com>
2019-05-07 17:33:39 +0100

    Update getLocationCDS.py

--- cgi-biocomp2/bl/getLocationCDS.py
+++ cgi-biocomp2/bl/getLocationCDS.py
@@ -12,17 +12,21 @@ Description: Function to getting Chromosome Location from the database
 --------------------------------------------------------------------------
 Import libraries: 
 from SQL_python_functions import db_query
-from configFile import *
+from config import config as cg
+import sys
 """
 from SQL_python_functions import db_query
-from configFile import *
+from config import config as cg
+import sys
+
+sys.path.insert(0, '../db/')
+sys.path.insert(0, '../')
 
 """CDS location"""
 def getLocationCDS(input_type, input_value):
      """ Function to obtain CDS Location from the database
      input: database query with Arg4, type and value from the config file
      output: CDS Location from database """
-     cds_location = db_query(dbArg6, input_value, output_value)
+     cds_location = db_query(cg.dbArg6, input_value, output_value)
      
      return cds_location
-
