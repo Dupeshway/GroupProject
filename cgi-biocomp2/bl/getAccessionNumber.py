c8a59708b8465e590e49e55b003dd0400aa4a0ab
mmarto27 <48446427+mmarto27@users.noreply.github.com>
2019-05-07 17:27:19 +0100

    Update getAccessionNumber.py

--- cgi-biocomp2/bl/getAccessionNumber.py
+++ cgi-biocomp2/bl/getAccessionNumber.py
@@ -10,17 +10,22 @@ Address: Biological Sciences, Birkbeck ...
 Description: Function for getting Accession Number from the database 
 --------------------------------------------------------------------------
 imported Libraries:
-from SQL_python_functions import *
-from configFile import *
+from SQL_python_functions import db_query
+from config import config as cg
+import sys
 """
 from SQL_python_functions import db_query
-from configFile import *
+from config import config as cg
+import sys
+
+sys.path.insert(0, '../db/')
+sys.path.insert(0, '../')
 
 """ Getting Accession Number"""
 def getAccessionNumber(input_type, input_value):
      """ Function to obtain accession number from the database
      input: database query with Arg4, type and value from the config file
      output: accession numberfrom database """
-     accession_number = db_query(dbArg1, input_type, input_value)
+     accession_number = db_query(cg.dbArg1, input_type, input_value)
 
      return accession_number
